f001270d70ee20354a97bd5e38ffac5472427ab8
Nozomu Ai <noah.fournis@gmail.com>
2026-01-12 12:13:38 +0100

    put previous test in hashtag, music infinite loop placeholder track

--- FX/SFX.py
+++ FX/SFX.py
@@ -4,8 +4,15 @@ import time
 pygame.init()
 pygame.mixer.init(44100, -16, 2, 2048)
 
-pew = pygame.mixer.Sound("bone-undertale-sound-effect.mp3")
-pew.play()
+pygame.mixer.music.load("bgm1.wav")
 
-# Wait for the sound to finish playing (for testng only, game will loop and won't need it)
-time.sleep(pew.get_length())  
+pew = pygame.mixer.Sound("bone-undertale-sound-effect.mp3")
+# pew.play()
+
+# Wait for the sound to finish playing (for testing only, game will loop and won't need it)
+# time.sleep(pew.get_length())  
+
+pygame.mixer.music.play(-1)  # Loop forever
+
+while True:
+    pygame.time.Clock().tick(10)
