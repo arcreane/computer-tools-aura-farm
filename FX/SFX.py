48f4cd713835f96b05077c3d561979c02288a1bc
Nozomu Ai <noah.fournis@gmail.com>
2026-01-12 12:45:31 +0100

    changed shooting sound, added sounds for button, dmg taken

--- FX/SFX.py
+++ FX/SFX.py
@@ -6,8 +6,11 @@ pygame.mixer.init(44100, -16, 2, 2048)
 
 pygame.mixer.music.load("bgm1.wav")
 
-pew = pygame.mixer.Sound("bone-undertale-sound-effect.mp3")
-# pew.play()
+button_click_sound = pygame.mixer.Sound("button_click.wav")
+pew_sound = pygame.mixer.Sound("pew.wav")
+dmg_taken_sound = pygame.mixer.Sound("dmg_taken.wav")
+
+# pew_sound.play()
 
 # Wait for the sound to finish playing (for testing only, game will loop and won't need it)
 # time.sleep(pew.get_length())  
